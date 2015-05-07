379466d4356235710f344b50acdab84e51d0cc3a
Mauro Scomparin <scompo@gmail.com>
2015-05-07 23:27:04 +0200

    removed useless parentheses

--- src/main/java/it/scompo/intervals/merge/utils/IntervalOverlapCheckImpl.java
+++ src/main/java/it/scompo/intervals/merge/utils/IntervalOverlapCheckImpl.java
@@ -12,8 +12,8 @@ public class IntervalOverlapCheckImpl<T extends Comparable<T>> implements Interv
 	@Override
 	public boolean overlappingIntervals(Interval<T> first, Interval<T> second) {
 
-		return (first.getStart().compareTo(second.getEnd()) <= 0)
-				&& (second.getStart().compareTo(first.getEnd()) <= 0);
+		return first.getStart().compareTo(second.getEnd()) <= 0
+				&& second.getStart().compareTo(first.getEnd()) <= 0;
 	}
 
 }
