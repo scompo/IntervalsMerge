080decc6784b792489d94812510893ce58aea583
Mauro Scomparin <scompo@gmail.com>
2015-05-07 22:41:03 +0200

    fixed testMax

--- src/main/java/it/scompo/intervals/merge/utils/ComparisonUtils.java
+++ src/main/java/it/scompo/intervals/merge/utils/ComparisonUtils.java
@@ -1,5 +1,7 @@
 package it.scompo.intervals.merge.utils;
 
+import static it.scompo.intervals.merge.utils.NullChekUtils.notNullElement;
+
 public class ComparisonUtils {
 
 	private ComparisonUtils() {
@@ -8,14 +10,22 @@ public class ComparisonUtils {
 
 	public static <T extends Comparable<? super T>> T max(T first, T second) {
 
-		if (first.compareTo(second) >= 0) {
-
-			return first;
-
-		} else {
-
-			return second;
+		T res = null;
+		
+		if (notNullElement(first) && notNullElement(second)) {
+			
+			if (first.compareTo(second) >= 0) {
+				
+				res =  first;
+				
+			} else {
+				
+				res = second;
+			}
 		}
+		
+		return res;
+		
 	}
 	
 	public static <T extends Comparable<? super T>> T min(T first, T second) {
--- src/test/java/it/scompo/intervals/merge/test/utils/TestComparisonUtils.java
+++ src/test/java/it/scompo/intervals/merge/test/utils/TestComparisonUtils.java
@@ -1,5 +1,7 @@
 package it.scompo.intervals.merge.test.utils;
 
+import static it.scompo.intervals.merge.utils.ComparisonUtils.max;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.After;
@@ -7,6 +9,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestComparisonUtils {
+
+	private static final Integer BIG = 100;
+	private static final Integer SMALL = 1;
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,7 +23,11 @@ public class TestComparisonUtils {
 
 	@Test
 	public void testMax() {
-		fail("Not yet implemented");
+
+		assertEquals(BIG, max(SMALL, BIG));
+		assertEquals(BIG, max(BIG, SMALL));
+		assertEquals(BIG, max(BIG, BIG));
+		assertEquals(null, max(null, null));
 	}
 
 	@Test
